ce53e99d1d619c0d2788f844722500ece5c89423
Francisco Pereira <fchamicapereira@gmail.com>
2018-02-12 18:45:22 +0000

    Adds schedule component (#4)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,5 +7,12 @@ export const environment = {
   production: false,
   deckUrl: 'https://deck.sinfo.org',
   cannonUrl: 'https://cannon.sinfo.org',
-  currentEvent: '25-sinfo'
+  currentEvent: '25-sinfo',
+  themes: {
+    26: 'Software',
+    27: 'Multimedia',
+    28: 'Security',
+    1: 'Games',
+    2: 'Web'
+  }
 }
