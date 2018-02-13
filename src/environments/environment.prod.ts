f78f11a31472f2fe1f50b24813618faa796594df
Carlos Faria <carlosfaria94@users.noreply.github.com>
2018-02-13 10:30:52 +0000

    Small clean in schedule  (#6)
    * Clean landing-page
    
    * Clean uncessary code
    
    * Change envirnoment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   deckUrl: 'https://deck.sinfo.org',
   cannonUrl: 'https://cannon.sinfo.org',
-  currentEvent: '25-sinfo',
+  currentEvent: '24-sinfo',
   themes: {
     26: 'Software',
     27: 'Multimedia',
