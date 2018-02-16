7d12cd9b37f37fa03eb56de162a4ee02a265cbc4
CarlosFaria94 <carlosfaria94@gmail.com>
2018-02-16 10:25:07 +0000

    Fix ngOnInit

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -19,14 +19,16 @@ export class UserComponent implements OnInit {
   ) { }
 
   ngOnInit () {
-    this.route.params.forEach((params: Params) => this.getUser(params['id']))
-    this.route.params.forEach((params: Params) => this.getUserAchievements(params['id']))
+    this.route.params.forEach((params: Params) => {
+      this.getUser(params['id'])
+      this.getUserAchievements(params['id'])
+    })
   }
 
   getUser (id: string): void {
     this.userService.getUser(id)
-      .subscribe(user => { 
-        this.user = user 
+      .subscribe(user => {
+        this.user = user
       })
   }
 
