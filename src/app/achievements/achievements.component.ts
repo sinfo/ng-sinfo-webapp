afc85d7aec4ae14a31a424531a4567e2a7cff6a9
Miguel Araújo <miguel.f.araujo@tecnico.ulisboa.pt>
2018-02-20 13:37:07 +0000

    Achievement unlocked html class

--- src/app/achievements/achievements.component.ts
+++ src/app/achievements/achievements.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 import { Router } from '@angular/router'
 
 import { Achievement } from './achievement.model'
 import { AchievementService } from './achievement.service'
+import { AuthService } from '../auth/auth.service'
+import { UserService } from '../user/user.service'
+import { User } from '../user/user.model'
 
 @Component({
   selector: 'app-achievements',
@@ -12,9 +15,12 @@ import { AchievementService } from './achievement.service'
 export class AchievementsComponent implements OnInit {
   
   achievements: Achievement[]
+  private user: User
 
   constructor(
     private achievementService: AchievementService,
+    private authService: AuthService,
+    private userService: UserService,
     private router: Router
   ) { }
 
@@ -24,11 +30,27 @@ export class AchievementsComponent implements OnInit {
 
   getAchievements (): void {
     this.achievementService.getAchievements()
-      .subscribe(achievements => this.achievements = achievements)
+      .subscribe(achievements => {
+        this.achievements = achievements 
+        this.getUser()
+      })
   }
 
   onSelect (achievement: Achievement): void {
-    this.router.navigate(['/achievement', achievement.id])
+    this.router.navigate(['/achievements', achievement.id])
+  }
+
+  getUser (): void {
+    let isLoggedIn = this.authService.isLoggedIn()
+
+    if (isLoggedIn) {
+      this.userService.getMe()
+        .subscribe(user => this.user = user)
+    }
+  }
+
+  isUnlocked (achievement: Achievement): boolean {
+    return this.user ? achievement.users.indexOf(this.user.id) !== -1 : false
   }
 
 }
