6f7e0999e958e9d0106ee0dfb74f0f76e1c9638b
Miguel Araújo <miguel.f.araujo@tecnico.ulisboa.pt>
2018-02-22 12:04:03 +0000

    Fixed schedule and about us scetion dropdowns

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent implements OnInit {
-
-  constructor () { }
+  selectedAboutText: string
+  displayAboutDropdown: boolean
 
   ngOnInit () {
+    this.selectedAboutText = 'About Us'
+    this.showOrHideDropdown()
   }
 
+  /* Beggining of Dropdown tabs actions */
+  showOrHideDropdown(): void {
+    this.displayAboutDropdown = window.innerWidth > 768 ? true : false
+  }
+
+  toggleDropdown(): void {
+    this.displayAboutDropdown = !this.displayAboutDropdown
+  }
+
+  updatedSelectedText(text: string): void {
+    this.selectedAboutText = text
+  }
+  /* End of Dropdown tabs actions */
 }
