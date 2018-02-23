f3be47fc6b89dbbb9e35fa6097f898bd0c0be92c
CarlosFaria94 <carlosfaria94@gmail.com>
2018-02-23 16:34:06 +0000

    Fix fragments

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, AfterViewInit } from '@angular/core'
 import { MessageService, Type } from '../message.service'
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router'
 
 @Component({
   selector: 'app-landing-page',
@@ -9,6 +10,24 @@ import { MessageService, Type } from '../message.service'
 export class LandingPageComponent implements OnInit {
   selectedAboutText: string
   displayAboutDropdown: boolean
+
+  constructor (
+    private router: Router
+  ) {
+    /**
+     * At time of writing this, there is no way to scroll to fragments, natively.
+     * Issue: https://github.com/angular/angular/issues/6595
+     */
+    router.events.subscribe(s => {
+      if (s instanceof NavigationEnd) {
+        const tree = router.parseUrl(router.url)
+        if (tree.fragment) {
+          const element = document.querySelector('#' + tree.fragment)
+          if (element) { element.scrollIntoView(true) }
+        }
+      }
+    })
+  }
 
   ngOnInit () {
     this.selectedAboutText = 'About Us'
