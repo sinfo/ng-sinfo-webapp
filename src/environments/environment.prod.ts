ec48d2f53b34cb5fc5b42d75b2f258661914848e
fchamicapereira <fchamicapereira@gmail.com>
2018-02-26 08:07:12 +0000

    cannon was running on localhost

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   deckUrl: 'https://deck.sinfo.org',
-  cannonUrl: 'http://localhost:8090',
+  cannonUrl: 'http://cannon.sinfo.org',
   currentEvent: '25-sinfo',
   themes: [
     'Software',
