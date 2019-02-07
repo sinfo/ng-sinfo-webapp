82dee84a2d1776eb6a12935e757abb8bea20c635
Pedro Correia <pedro.correia.105@gmail.com>
2019-02-07 15:56:40 +0000

    last editions no longer hardcoded

--- src/app/partials/menu/menu.component.ts
+++ src/app/partials/menu/menu.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core'
 import { Router } from '@angular/router'
 import { AuthService } from '../../auth/auth.service'
 import { MessageService } from '../../message.service'
+import { EventService } from '../../events/event.service'
+import { Event } from '../../events/event.model'
+import { environment } from '../../../environments/environment.prod';
 
 @Component({
   selector: 'app-menu',
@@ -10,19 +13,34 @@ import { MessageService } from '../../message.service'
 })
 export class MenuComponent implements OnInit {
   isLoggedIn = false
+  private shortEventList: Event[]
 
   constructor (
     private router: Router,
     public messageService: MessageService,
+    public eventService: EventService,
     private authService: AuthService
   ) { }
 
   ngOnInit () {
     this.isLoggedIn = this.authService.isLoggedIn()
+    this.getShortEventList()
   }
 
   onLogout (): void {
     this.authService.logout()
     this.router.navigate(['/'])
+  }
+
+  getShortEventList (): void {
+    this.eventService.getEvents().subscribe(events => {
+      //Sort array
+      this.shortEventList = events.sort(function(a,b){
+        return Date.parse(b.date) - Date.parse(a.date);
+      //Remove current event
+      }).filter(function(a){
+        return a.id != environment.currentEvent
+      })
+    })
   }
 }
