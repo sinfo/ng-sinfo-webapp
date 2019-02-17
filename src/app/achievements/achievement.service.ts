6ee93336ebbe3f21a2eb8280f98ec3494156767a
Pedro Correia <pedro.correia.105@gmail.com>
2019-02-17 12:06:32 +0000

    Fix schedule (#142)
    * fixed schedule
    
    * fixed width 100

--- src/app/achievements/achievement.service.ts
+++ src/app/achievements/achievement.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core'
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http'
-
-import { Observable , of } from 'rxjs'
+import { Observable, of } from 'rxjs'
 import { catchError, tap } from 'rxjs/operators'
-
 import { environment } from '../../environments/environment'
 
 import { Achievement } from './achievement.model'
