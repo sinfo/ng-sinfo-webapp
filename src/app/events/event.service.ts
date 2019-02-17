78faacfec4c016b80ec9bd56202dcd7df4e5be1d
Pedro Correia <pedro.correia.105@gmail.com>
2019-02-17 16:26:18 +0000

    Scroll fix (#144)
    * several improvements
    
    * another scroll fix

--- src/app/events/event.service.ts
+++ src/app/events/event.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core'
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 
-import { Observable ,  of } from 'rxjs'
+import { Observable, of } from 'rxjs'
 import { catchError, map, tap, take, filter } from 'rxjs/operators'
 
 import { environment } from '../../environments/environment'
