c7e3f6033d7368816f0f421170eb60ac0c98b2cb
Pedro Correia <pedro.correia.105@gmail.com>
2019-10-04 15:09:35 +0100

    reverted dynamic meta tags because crawlers dont see it, mailto button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core'
-import { Meta, Title } from '@angular/platform-browser'
-import { EventService } from './events/event.service'
-
 import { Router, NavigationEnd } from '@angular/router'
 
 @Component({
@@ -11,58 +8,8 @@ import { Router, NavigationEnd } from '@angular/router'
 })
 export class AppComponent {
   constructor (
-    private router: Router,
-    private titleService: Title,
-    private meta: Meta,
-    private eventService: EventService
+    private router: Router
   ) {
-
-    eventService.getCurrent().subscribe(event => {
-
-      const DESCRIPTION = 'SINFO is a non-profit, college student, organization responsible \
-      for organizing one of the biggest Tech conferences in Portugal.'
-      const IMAGE = 'assets/img/thumbnail-sinfo27.png'
-      const TITLE = event.name
-
-      // Set pages title
-      titleService.setTitle(TITLE)
-
-      // Meta data tags
-      // Common tags
-      meta.addTags([
-        { name: 'description', content: DESCRIPTION },
-        { name: 'author', content: 'The SINFO Organization' },
-        { name: 'image', content: IMAGE }
-      ])
-
-      // Twitter
-      meta.addTags([
-        { name: 'twitter:site', content: '@sinfoist' },
-        { name: 'twitter:title', content: TITLE },
-        { name: 'twitter:description', content: DESCRIPTION },
-        { name: 'twitter:card', content: 'summary' },
-        { name: 'twitter:image:src', content: IMAGE }
-      ])
-
-      // Schema.org for Google
-      meta.addTags([
-        { itemprop: 'name', content: TITLE },
-        { itemprop: 'description', content: DESCRIPTION },
-        { itemprop: 'image', content: IMAGE }
-      ])
-
-      // Open Graph general (Facebook, Pinterest & Google+)
-      meta.addTags([
-        { name: 'og:site_name', content: TITLE },
-        { name: 'og:title', content: TITLE },
-        { name: 'og:description', content: DESCRIPTION },
-        { name: 'og:image', content: IMAGE },
-        { name: 'og:url', content: 'https://sinfo.org' },
-        { name: 'og:locale', content: 'en_US' },
-        { name: 'og:admins', content: '134758406571284' },
-        { name: 'og:type', content: 'website' }
-      ])
-    })
 
     /**
      * At time of writing this, there is no way to scroll to fragments, natively.
