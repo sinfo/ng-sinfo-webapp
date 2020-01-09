dfde96f740e262bdf4aad40a95c11813286aa708
Pedro Maximino <53193337+PMax5@users.noreply.github.com>
2020-01-09 18:47:09 +0000

    Added feedback slideshow to website.

--- src/app/landing-page/feedback/feedback.component.ts
+++ src/app/landing-page/feedback/feedback.component.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from '@angular/core'
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap'
 
 import { EventService } from '../../events/event.service'
 
 @Component({
   selector: 'app-feedback',
   templateUrl: './feedback.component.html',
-  styleUrls: ['./feedback.component.css']
+  styleUrls: ['./feedback.component.css'],
+  providers: [NgbCarouselConfig]
 })
+
+
 export class FeedbackComponent implements OnInit {
 
   show: boolean
+  images = [700, 533, 807, 124].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  constructor(private eventService: EventService, config: NgbCarouselConfig) {
+    config.interval = 10000;
+    config.wrap = false;
+    config.keyboard = false;
+    config.pauseOnHover = false;
+  }
 
-  constructor (private eventService: EventService) { }
-
-  ngOnInit () {
+  ngOnInit() {
     this.eventService.getCurrent().subscribe(event => {
       this.show = !event.isOcurring
     })
