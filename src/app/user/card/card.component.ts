69ef584d12e2e698e1ad86028e73745ae4647b08
jaga8285 <vitamina@tangerina.org>
2021-02-26 12:39:56 +0000

    card is now pretty

--- src/app/user/card/card.component.ts
+++ src/app/user/card/card.component.ts
@@ -15,11 +15,10 @@ import { EventService } from '../../events/event.service'
 export class CardComponent implements OnInit {
 
   user: User
-  signatures = {
-    day: new Date(),
-    capacity: environment.signaturesCardCapacity,
-    companies: []
-  }
+  capacity = environment.signaturesCardCapacity
+  day= new Date()
+  companies = []
+  userSignatures = []
 
   constructor (
     private userService: UserService,
@@ -33,33 +32,90 @@ export class CardComponent implements OnInit {
       this.titleService.setTitle(event.name + ' - Card')
 
       this.userService.getMe().subscribe(user => {
-        this.user = userMM
+        this.user = user
 
         if (!this.user.signatures) {
           return
         }
 
-        let day = this.signatures.day.getDate().toString()
-        let userSignatures = this.user.signatures.find(s => {
+        let day = this.day.getDate().toString()
+        let tempSignatures = this.user.signatures.find(s => {
           return s.day === day && s.edition === event.id
-        }) || { day: day, edition: event.id, redeemed: false, signatures: [] }
+        }) || {signatures: [] }
 
-        userSignatures.signatures.sort((a,b) => {return b.date.valueOf() - a.date.valueOf()})
-        userSignatures.signatures = userSignatures.signatures.slice(0, this.signatures.capacity)
-        userSignatures.signatures = [{ companyId:"optimus", date: new Date()}, { companyId:"optimus", date: new Date()}, { companyId:"optimus", date: new Date()}, { companyId:"optimus", date: new Date()}, { companyId:"optimus", date: new Date()}, { companyId:"optimus", date: new Date()}]
+        this.userSignatures=tempSignatures.signatures
+        
+        this.userSignatures = [
+          { companyId:"optimus", date: new Date(new Date().setHours(2))}, 
+          { companyId:"epimetheus", date: new Date(new Date().setHours(8))}, 
+          { companyId:"altice", date: new Date(new Date().setHours(9))}, 
+          { companyId:"talkdesk", date: new Date(new Date().setHours(1))}, 
+          { companyId:"miniclip", date: new Date(new Date().setHours(11))}
+        ]
+        this.userSignatures.sort((a,b) => {return b.date.valueOf() - a.date.valueOf()})
+        this.userSignatures = this.userSignatures.slice(0, this.capacity)
 
-        userSignatures.signatures.forEach(signature => {
+        this.userSignatures.forEach((signature) => {
+          this.companies.push({
+            name: signature.companyId,
+            img: "",
+            howLongAgo: this.formatDuration((new Date().valueOf() - signature.date.valueOf())/1000) + " ago"
+          })
+        })
+
+        this.userSignatures.forEach(signature => {
           this.companyService.getCompany(signature.companyId)
             .subscribe(c => {
-              this.signatures.companies.push({
-                name: c.name,
-                img: c.img
-              })
-            })
+              this.companies[this.companies.findIndex((company)=>{ return company.name === c.id})].img = c.img
+          })
         })
+        console.log(this.companies)
+        console.log(this.userSignatures)
       })
     })
 
   }
+  formatDuration(seconds) {
+    if (seconds === 0) {
+        return 'now';
+    }
+    var numYears = Math.floor(seconds / (86400 * 365));
+    var numDays = Math.floor(seconds % (86400 * 365) / 86400);
+    var numHours = Math.floor(((seconds % (86400 * 365)) % 86400) / 3600);
+    var numMinutes = Math.floor((((seconds % (86400 * 365)) % 86400) % 3600) / 60);
+    var numSeconds = (((seconds % (86400 * 365)) % 86400) % 3600) % 60;
+
+    var secondsFun = function () {
+        return (numSeconds ? (((seconds > 60) ? ' and ' + numSeconds + " second" : +numSeconds + " second") + (numSeconds === 1 ? '' : 's')) : '');
+    };
+
+    var minutesFun = function () {
+        if (numSeconds === 0 && numDays !== 0) {
+            return 'and ' + (numMinutes ? numMinutes + " minute" + (numMinutes === 1 ? '' : 's') : '');
+        }
+        return (numMinutes ? numMinutes + " minute" + (numMinutes === 1 ? '' : 's') : '');
+    };
+
+    var hoursFun = function () {
+        if (minutesFun().charAt(0) === 'a') {
+            return (numHours ? numHours + " hour" + (numHours === 1 ? ' ' : 's ') : '');
+        }
+        if (numSeconds === 0 && numMinutes === 0) {
+            return (numHours ? numHours + " hour" + (numHours === 1 ? '' : 's') : '');
+        }
+
+        return (numHours ? numHours + " hour" + (numHours === 1 ? ' ' : 's ') : '');
+    };
+
+    var daysFun = function () {
+        return (numDays ? numDays + " day" + (numDays === 1 ? ' ' : 's ') : '');
+    };
+
+    var yearsFun = function () {
+        return (numYears ? numYears + " year" + (numYears === 1 ? ' ' : 's ') : '');
+    };
+
+    return yearsFun() || daysFun() || hoursFun() || minutesFun() || secondsFun();
+}
 
 }
