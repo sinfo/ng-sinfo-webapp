4c797027eafa944e5ae682c2cf026b0917712da7
Dpac99 <diogo.a.pacheco@tecnico.ulisboa.pt>
2022-03-07 11:23:03 -0100

    fixes google logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -29,14 +29,14 @@ export class AuthService {
   linkedinState: string
   loginType: LoginType
 
-  constructor (
+  constructor(
     private http: HttpClient,
     private messageService: MessageService,
     private storageService: StorageService,
     private jwtService: JwtService,
   ) { }
 
-  facebook (id, token): Observable<CannonToken> {
+  facebook(id, token): Observable<CannonToken> {
     this.loginType = LoginType.FACEBOOK
     return this.http.post<CannonToken>(`${this.authUrl}/facebook`, { id, token }, httpOptions)
       .pipe(
@@ -45,7 +45,7 @@ export class AuthService {
       )
   }
 
-  google (id, token): Observable<CannonToken> {
+  google(id, token): Observable<CannonToken> {
     this.loginType = LoginType.GOOGLE
     return this.http.post<CannonToken>(`${this.authUrl}/google`, { id, token }, httpOptions)
       .pipe(
@@ -63,7 +63,7 @@ export class AuthService {
       )
   }
 
-  linkedin (code): Observable<CannonToken> {
+  linkedin(code): Observable<CannonToken> {
     this.loginType = LoginType.LINKEDIN
     return this.http.post<CannonToken>(`${this.authUrl}/linkedin`, { code }, httpOptions)
       .pipe(
@@ -72,15 +72,15 @@ export class AuthService {
       )
   }
 
-  getToken (): CannonToken | null | undefined {
+  getToken(): CannonToken | null | undefined {
     return this.storageService.getItem('cannon_token') as CannonToken
   }
 
-  setToken (token: CannonToken): void {
+  setToken(token: CannonToken): void {
     this.storageService.setItem('cannon_token', token)
   }
 
-  isLoggedIn (): boolean {
+  isLoggedIn(): boolean {
     const cannonToken = this.getToken()
     if (!cannonToken) {
       return false
@@ -93,27 +93,28 @@ export class AuthService {
     return !isTokenExpired
   }
 
-  logout (): void {
+  logout(): void {
+    const isGoogleActive = (typeof (gapi) !== 'undefined' && gapi !== null)
+
+    const cannonToken = this.getToken()
     this.storageService.removeItem('cannon_token')
-    
-    //TODO: finish log out
-    // switch (this.loginType) {
-    //   case LoginType.FACEBOOK:
-    //     FB.logout();
-    //     break;
-    //   case LoginType.GOOGLE:
-    //     let auth2 = gapi.auth2.getAuthInstance();
-    //     auth2.signOut().then(function () {
-    //       auth2.disconnect();
-    //     });
-    //     break;
-    //   case LoginType.LINKEDIN:
-    //     break;
-    //   case LoginType.FENIX:
-    //     break;
-    //   default:
-    //     break;
-    // }
+
+    switch (cannonToken.loginWith) {
+      case 'facebook':
+        FB.logout();
+        break;
+      case 'google':
+        if (isGoogleActive) {
+          gapi.auth2.getAuthInstance().disconnect();
+        }
+        break;
+      case 'linkedin':
+        break;
+      case 'fenix':
+        break;
+      default:
+        break;
+    }
   }
 
   /**
@@ -122,7 +123,7 @@ export class AuthService {
    * @param operation - name of the operation that failed
    * @param result - optional value to return as the observable result
    */
-  private handleError<T> (operation = 'operation', result?: T) {
+  private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       this.messageService.add({
         origin: `AuthService: ${operation}`,
