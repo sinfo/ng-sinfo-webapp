cf6b6989e192de5372bea21d8e53f94fc4e9d4dd
bvlourenco <bvlourenco5@gmail.com>
2023-01-28 16:54:23 +0000

    Google login in javascript

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,37 +1,36 @@
-import { Component, OnInit, AfterViewInit, NgZone } from '@angular/core'
-import { Router, ActivatedRoute } from '@angular/router'
-import { Title } from '@angular/platform-browser'
+import { Component, OnInit, AfterViewInit, NgZone } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { Title } from "@angular/platform-browser";
 
-import { environment } from '../../../environments/environment'
-import { AuthService } from '../auth.service'
-import { MessageService, Type } from '../../message.service'
-import { EventService } from '../../events/event.service'
-import { MatSnackBar } from '@angular/material/snack-bar'
+import { environment } from "../../../environments/environment";
+import { AuthService } from "../auth.service";
+import { MessageService, Type } from "../../message.service";
+import { EventService } from "../../events/event.service";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
-declare let FB: any
-declare let gapi: any
+declare let FB: any;
+declare let google: any;
 
-let GoogleAuth
-let GOOGLE_SCOPE = 'profile email openid'
+// let GoogleAuth;
+// let GOOGLE_SCOPE = "profile email openid";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit, AfterViewInit {
-  isFacebookActive: boolean
-  isGoogleActive: boolean
+  isFacebookActive: boolean;
+  isGoogleActive: boolean;
 
-  submitting = false
+  submitting = false;
 
-  fenixUrlAuth =
-    `https://fenix.tecnico.ulisboa.pt/oauth/userdialog?client_id=${environment.fenix.clientId}&redirect_uri=${environment.fenix.redirectUrl}`
+  fenixUrlAuth = `https://fenix.tecnico.ulisboa.pt/oauth/userdialog?client_id=${environment.fenix.clientId}&redirect_uri=${environment.fenix.redirectUrl}`;
 
   // tslint:disable-next-line:max-line-length
-  linkedinUrlAuth = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${environment.linkedin.clientId}&redirect_uri=${environment.linkedin.redirectUrl}&state=SINFO&scope=r_liteprofile%20r_emailaddress`
+  linkedinUrlAuth = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${environment.linkedin.clientId}&redirect_uri=${environment.linkedin.redirectUrl}&state=SINFO&scope=r_liteprofile%20r_emailaddress`;
 
-  private isLoggedIn = false
+  private isLoggedIn = false;
 
   constructor(
     private messageService: MessageService,
@@ -42,36 +41,41 @@ export class LoginComponent implements OnInit, AfterViewInit {
     private zone: NgZone,
     private titleService: Title,
     private snackBar: MatSnackBar
-
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.eventService.getCurrent().subscribe(event => {
-      this.titleService.setTitle(event.name + ' - Login')
-    })
+    this.eventService.getCurrent().subscribe((event) => {
+      this.titleService.setTitle(event.name + " - Login");
+    });
 
-    this.isLoggedIn = this.authService.isLoggedIn()
+    this.isLoggedIn = this.authService.isLoggedIn();
 
     if (this.isLoggedIn) {
-      this.router.navigate([`${this.authService.redirectUrl || '/user/qrcode'}`])
-      return
+      this.router.navigate([
+        `${this.authService.redirectUrl || "/user/qrcode"}`,
+      ]);
+      return;
     }
 
-    this.route.queryParams.subscribe(params => {
-      const fenixCode = params['code']
+    this.route.queryParams.subscribe((params) => {
+      const fenixCode = params["code"];
       if (fenixCode) {
-        this.onFenixLogin(fenixCode)
+        this.onFenixLogin(fenixCode);
       }
-    })
+    });
 
-    this.isFacebookActive = (typeof (FB) !== 'undefined' && FB !== null)
-    this.isGoogleActive = (typeof (gapi) !== 'undefined' && gapi !== null)
+    this.isFacebookActive = typeof FB !== "undefined" && FB !== null;
+    this.isGoogleActive = typeof google !== "undefined" && google !== null;;
 
     if (!this.isFacebookActive) {
-      this.snackBar.open(`You need to disable any ad blocker or tracking protection mechanism to be allowed to login with Google.`, "Ok", {
-        panelClass: ['mat-toolbar', 'mat-primary'],
-        duration: 2000
-      })
+      this.snackBar.open(
+        `You need to disable any ad blocker or tracking protection mechanism to be allowed to login with Google.`,
+        "Ok",
+        {
+          panelClass: ["mat-toolbar", "mat-primary"],
+          duration: 2000,
+        }
+      );
       /* this.messageService.add({
         origin: `LoginComponent: ngOnInit isFacebookActive=${this.isFacebookActive}`,
         text: `You need to disable any ad blocker or tracking protection mechanism to be
@@ -82,7 +86,7 @@ export class LoginComponent implements OnInit, AfterViewInit {
       }) */
     }
     if (!this.isGoogleActive) {
-      this.snackBar.open(`You need to disable any ad blocker or tracking protection mechanism to be allowed to login with Facebook.`, "Ok", {
+      this.snackBar.open(`You need to disable any ad blocker or tracking protection mechanism to be allowed to login with Google.`, "Ok", {
         panelClass: ['mat-toolbar', 'mat-primary'],
         duration: 2000
       })
@@ -98,97 +102,104 @@ export class LoginComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    this.initSocialSDKs()
+    this.initSocialSDKs();
   }
 
   initSocialSDKs() {
     if (this.isGoogleActive) {
+      google.accounts.id.initialize({
+        client_id: environment.google.clientId,
+        callback: this.handleGoogleCredentialResponse.bind(this),
+        auto_select: false,
+        cancel_on_tap_outside: true,
+      });
+      google.accounts.id.renderButton(
+        document.getElementById("google-button"),
+        {
+          theme: "outline",
+          size: "large",
+          type: "standard",
+          shape: "rectangular",
+          text: "signin_with",
+          logo_alignment: "left"
+        }
+      );
+      // google.accounts.id.prompt((notification: PromptMomentNotification) => {});
+      google.accounts.id.prompt();
 
-      gapi.load('auth2', () => {
-        gapi.auth2.init({
-          client_id: environment.google.clientId,
-          cookiepolicy: 'single_host_origin',
-          scope: GOOGLE_SCOPE
-        }).then(() => {
-          try {
-            GoogleAuth = gapi.auth2.getAuthInstance()
 
-            // Listen for sign-in state changes.
-            // GoogleAuth.isSignedIn.listen(isSignedIn => { this.onGoogleListen(isSignedIn) })
-          } catch (err) {
-            console.log(err)
-          }
-        })
-      })
+      // gapi.load('auth2', () => {
+      //   gapi.auth2.init({
+      //     client_id: environment.google.clientId,
+      //     cookiepolicy: 'single_host_origin',
+      //     scope: GOOGLE_SCOPE
+      //   }).then(() => {
+      //     try {
+      //       GoogleAuth = gapi.auth2.getAuthInstance()
+
+      //       // Listen for sign-in state changes.
+      //       // GoogleAuth.isSignedIn.listen(isSignedIn => { this.onGoogleListen(isSignedIn) })
+      //     } catch (err) {
+      //       console.log(err)
+      //     }
+      //   })
+      // })
     }
     if (this.isFacebookActive) {
       FB.init({
         appId: environment.facebook.appId,
         cookie: false,
         xfbml: true,
-        version: 'v5.0'
-      })
+        version: "v5.0",
+      });
     }
+  }
+
+  async handleGoogleCredentialResponse(response: any) {
+    // Here will be your response from Google.
+    console.log(response);
   }
 
   onFenixLogin(fenixCode) {
-    this.submitting = true
-    this.authService.fenix(fenixCode).subscribe(cannonToken => {
-      this.authService.setToken(cannonToken)
-      this.zone.run(() => this.router.navigate([`${this.authService.redirectUrl || '/user/qrcode'}`]))
-    })
-  }
-
-  onGoogleListen(isSignedIn: boolean) {
-    if (!isSignedIn) { GoogleAuth.signIn(); return }
-
-    let googleUser = GoogleAuth.currentUser.get()
-
-    const profile = googleUser.getBasicProfile()
-    const userId = profile.getId()
-    const token = googleUser.getAuthResponse().id_token
-
-    this.authService.google(userId, token).subscribe(
-      cannonToken => {
-        this.authService.setToken(cannonToken)
-        this.zone.run(() => this.router.navigate([`${this.authService.redirectUrl || '/user/qrcode'}`]))
-      },
-      error => {
-        console.error(error)
-        this.zone.run(() => this.router.navigate(['/login']))
-      })
-  }
-
-  onGoogleLogin() {
-    this.submitting = true
-
-    if (GoogleAuth.isSignedIn.get()) {
-      this.onGoogleListen(true)
-    } else {
-      GoogleAuth.signIn().then(isSignedIn => { this.onGoogleListen(isSignedIn) })
-    }
+    this.submitting = true;
+    this.authService.fenix(fenixCode).subscribe((cannonToken) => {
+      this.authService.setToken(cannonToken);
+      this.zone.run(() =>
+        this.router.navigate([
+          `${this.authService.redirectUrl || "/user/qrcode"}`,
+        ])
+      );
+    });
   }
 
   onFacebookLogin() {
-    this.submitting = true
-    FB.login(response => {
-      this.facebookStatusChange(response)
-    }, { scope: 'public_profile,email' })
+    this.submitting = true;
+    FB.login(
+      (response) => {
+        this.facebookStatusChange(response);
+      },
+      { scope: "public_profile,email" }
+    );
   }
 
   facebookStatusChange(resp) {
-    if (resp.status === 'connected') {
+    if (resp.status === "connected") {
       // connect here with your server for facebook login by passing access token given by facebook
-      this.authService.facebook(resp.authResponse.userID, resp.authResponse.accessToken)
-        .subscribe(cannonToken => {
-          this.authService.setToken(cannonToken)
-          this.zone.run(() => this.router.navigate([`${this.authService.redirectUrl || '/user/qrcode'}`]))
-        })
-    } else if (resp.status === 'not_authorized') {
+      this.authService
+        .facebook(resp.authResponse.userID, resp.authResponse.accessToken)
+        .subscribe((cannonToken) => {
+          this.authService.setToken(cannonToken);
+          this.zone.run(() =>
+            this.router.navigate([
+              `${this.authService.redirectUrl || "/user/qrcode"}`,
+            ])
+          );
+        });
+    } else if (resp.status === "not_authorized") {
       this.snackBar.open(`You were not allowed to login with Facebook`, "Ok", {
-        panelClass: ['mat-toolbar', 'mat-warn'],
-        duration: 2000
-      })
+        panelClass: ["mat-toolbar", "mat-warn"],
+        duration: 2000,
+      });
       /* this.messageService.add({
         origin: `LoginComponent: facebookStatusChange: ${resp.status}`,
         text: 'You were not allowed to login with Facebook',
@@ -198,9 +209,9 @@ export class LoginComponent implements OnInit, AfterViewInit {
       }) */
     } else {
       this.snackBar.open(`An error occurred by logging with Facebook`, "Ok", {
-        panelClass: ['mat-toolbar', 'mat-warn'],
-        duration: 2000
-      })
+        panelClass: ["mat-toolbar", "mat-warn"],
+        duration: 2000,
+      });
       /* this.messageService.add({
         origin: `LoginComponent: facebookStatusChange: ${resp.status}`,
         text: 'An error occurred by logging with Facebook',
