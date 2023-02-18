067e7755d99bb98de6c62aa92a0bb5c1b8eee927
bvlourenco <bvlourenco5@gmail.com>
2023-02-18 15:59:51 +0000

    feat: Beggining of add achievement page #304

---
+++ src/app/user/add-achievement/add-achievement.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-achievement',
+  templateUrl: './add-achievement.component.html',
+  styleUrls: ['./add-achievement.component.css']
+})
+export class AddAchievementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
