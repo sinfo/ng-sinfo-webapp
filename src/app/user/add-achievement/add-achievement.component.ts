b3deb8e7a325438a79cb9e62f6ce9dec32c30f64
bvlourenco <bvlourenco5@gmail.com>
2023-02-19 00:30:04 +0000

    feat: Form to add info done #304

--- src/app/user/add-achievement/add-achievement.component.ts
+++ src/app/user/add-achievement/add-achievement.component.ts
@@ -1,16 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { CreateAchievement } from "./add-achievement.model";
+import { DateAdapter } from "@angular/material/core";
 
 @Component({
-  selector: 'app-add-achievement',
-  templateUrl: './add-achievement.component.html',
-  styleUrls: ['./add-achievement.component.css']
+  selector: "app-add-achievement",
+  templateUrl: "./add-achievement.component.html",
+  styleUrls: ["./add-achievement.component.css"],
 })
 export class AddAchievementComponent implements OnInit {
+  submitted: Boolean;
+  achievement: CreateAchievement;
 
-  constructor() { }
+  kinds = [
+    { value: "stand", viewValue: "Stand" },
+    { value: "presentation", viewValue: "Presentation" },
+    { value: "workshop", viewValue: "Workshop" },
+    { value: "keynote", viewValue: "Keynote" },
+    { value: "cv", viewValue: "CV" },
+    { value: "secret", viewValue: "Secret" },
+    { value: "standDay", viewValue: "Stand Day" },
+    { value: "other", viewValue: "Other" },
+  ];
 
-  ngOnInit(): void {
+  constructor(private dateAdapter: DateAdapter<Date>) {
+    this.submitted = false;
+    this.dateAdapter.setLocale("en-GB"); //dd/MM/yyyy
+    const validity = {
+      from: new Date(),
+      to: new Date()
+    }
+    this.achievement = new CreateAchievement("", "", "30-sinfo", "", 0, validity, "", "", "", "");
   }
 
-}
+  ngOnInit(): void {}
 
+  submitAchievement(form: any) {
+    this.submitted = true;
+    console.log(form);
+  }
+
+  uploadImage(event) {}
+}
